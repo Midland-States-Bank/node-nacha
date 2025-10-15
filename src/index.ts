5223d7684b63424c28a732185b7f5c18d3f7c40f
Grant McPherson <gmcpherson@midlandsb.com>
2025-10-15 09:29:10 -0500

    Export helper types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 import Nacha from "./api/Nacha.js";
-import { NachaData } from "./types/Nacha/index.js";
+import { NachaData, SEC } from "./types/Nacha/index.js";
 import { default as parse } from "./utils/parse/parseNacha.js";
 import { default as format } from "./utils/formatNacha.js";
+import {
+  AccountType,
+  Direction,
+  Purpose,
+  TransactionCode,
+} from "./types/EntryHelpers.js";
 
+// Export Class
 export default Nacha;
+
+// Export Helper Funcs
 export { parse, format };
-export type { NachaData };
+
+// Export Types
+export type {
+  NachaData,
+  TransactionCode,
+  SEC,
+  AccountType,
+  Direction,
+  Purpose,
+};
